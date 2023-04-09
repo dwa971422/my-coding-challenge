d2721abdc931e33135391371617b886ae7fd2d6e
Shih Yao Wang <sywang@sykenwang-MBA.local>
2023-04-09 00:04:22 -0500

    implement MyBlockingQueue using ReentrantLock and Condition

--- src/main/java/threadpoolchallenge/MyBlockingQueue.java
+++ src/main/java/threadpoolchallenge/MyBlockingQueue.java
@@ -2,37 +2,56 @@ package threadpoolchallenge;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class MyBlockingQueue<T> {
     private final Queue<T> queue;
     private final int MAX_TASK_COUNT;
+    private final Lock lock;
+    private final Condition addCondition;
+    private final Condition pollCondition;
 
     public MyBlockingQueue(int taskCount) {
         this.queue = new LinkedList<>();
         this.MAX_TASK_COUNT = taskCount;
+        this.lock = new ReentrantLock();
+        this.addCondition = this.lock.newCondition();
+        this.pollCondition = this.lock.newCondition();
     }
 
-    public synchronized void add(T task) throws InterruptedException {
-        while (queue.size() == MAX_TASK_COUNT) {
-            wait();
-        }
+    public void add(T task) throws InterruptedException {
+        lock.lock();
 
-        queue.add(task);
+        try {
+            while (queue.size() == MAX_TASK_COUNT) {
+                addCondition.await();
+            }
 
-        if (queue.size() == 1) {
-            notifyAll();
+            queue.add(task);
+
+            pollCondition.signalAll();
+        } finally {
+            lock.unlock();
         }
     }
 
-    public synchronized T poll() throws InterruptedException {
-        while (queue.isEmpty()) {
-            wait();
-        }
+    public T poll() throws InterruptedException {
+        lock.lock();
 
-        if (queue.size() == MAX_TASK_COUNT) {
-            notifyAll();
-        }
+        try {
+            while (queue.isEmpty()) {
+                pollCondition.await();
+            }
 
-        return queue.poll();
+            T task = queue.poll();
+
+            addCondition.signalAll();
+
+            return task;
+        } finally {
+            lock.unlock();
+        }
     }
 }
